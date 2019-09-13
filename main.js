d9a1134d105c6579beeed36adab1daeeb20ceb8d
Slobodan Miric <slobodan@Slobodans-MacBook-Pro.local>
2019-09-13 18:00:36 +0200

    nav jquery

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  /* navbar toggler */
+
+  $('.navbar-toggler').html("<i class='fas fa-bars fa-3x' style='color:var(--orange)'></i>");
+
+  // smooth scroll
+
+
+
+
+});
