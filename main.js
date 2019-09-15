5b9050a495b7257d191843809367aa51236c0554
Slobodan Miric <slobodan@Slobodans-MacBook-Pro.local>
2019-09-15 11:03:38 +0200

    numbers people skills

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-  /* navbar toggler */
+  /* navbar toggler, dodavanje ikonice za hamburger meni */
 
   $('.navbar-toggler').html("<i class='fas fa-bars fa-3x' style='color:var(--orange)'></i>");
 
   // smooth scroll
-
-
 
 
 });
